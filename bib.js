887915b108ffafbbeb08afe5a84dace78e2f7e75
David Karger <karger@mit.edu>
2019-07-31 00:42:16 -0400

    Created file

---
+++ bib.js
@@ -0,0 +1,12 @@
+{
+	"showbib": true,
+	"reference": [
+		{
+			"link": "http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.46.6393&rep=rep1&type=pdf",
+			"title": "Active tioga documents: an exploration of two paradigms",
+			"notes": "This is the ancient paper that introduced the idea of active documents.",
+			"bibtex": "@article{Terry:ActiveTioga,\n author = {Terry, Douglas B. and Baker, Donald G.},\n title = {Active tioga documents: an exploration of two paradigms},\n journal = {Electronic Publishing---Origination, Dissemination, and Design},\n volume = {3},\n number = {2},\n year = {1990},\n issn = {0894-3982},\n pages = {105--122},\n publisher = {John Wiley and Sons Ltd.},\n address = {Chichester, UK},\n}\n"
+		},
+		{}
+	]
+}
