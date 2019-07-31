7180f6cd89f7f8ae18c271e0ccf55573c33e701c
David Karger <karger@mit.edu>
2019-07-31 01:02:41 -0400

    Updated bib.js

--- bib.js
+++ bib.js
@@ -7,6 +7,18 @@
 			"notes": "This is the ancient paper that introduced the idea of active documents.",
 			"bibtex": "@article{Terry:ActiveTioga,\n author = {Terry, Douglas B. and Baker, Donald G.},\n title = {Active tioga documents: an exploration of two paradigms},\n journal = {Electronic Publishing---Origination, Dissemination, and Design},\n volume = {3},\n number = {2},\n year = {1990},\n issn = {0894-3982},\n pages = {105--122},\n publisher = {John Wiley and Sons Ltd.},\n address = {Chichester, UK},\n}\n"
 		},
-		{}
+		{},
+		{
+			"link": "http://doi.acm.org/10.1145/146486.146547",
+			"title": "EmbeddedButtons: supporting buttons in documents",
+			"notes": "A related early paper on active documents",
+			"bibtex": "\n@article{Bier:EmbeddedButtons,\n author = {Bier, Eric A.},\n title = {EmbeddedButtons: supporting buttons in documents},\n journal = {ACM Transactions on Information Systems},\n volume = {10},\n number = {4},\n year = {1992},\n issn = {1046-8188},\n pages = {381--407},\n doi = {http://doi.acm.org/10.1145/146486.146547},\n publisher = {ACM},\n address = {New York, NY, USA},\n }\n"
+		},
+		{
+			"link": "http://people.csail.mit.edu/karger/Papers/dido.pdf",
+			"title": "The web page as a WYSIWYG end-user customizable database-backed information management application ",
+			"notes": "This system implements an active, data bearing document with a built in editor for the data and also a visual editor for the document itself.  ",
+			"bibtex": "\n@inproceedings{Karger:DIDO,\n author = {Karger, David R. and Ostler, Scott and Lee, Ryan},\n title = {The web page as a WYSIWYG end-user customizable\n                  database-backed information management application},\n booktitle = {UIST '09: Proceedings of the 22nd annual ACM symposium\n                  on User interface software and technology},\n year = {2009},\n month = oct,\n isbn = {978-1-60558-745-5},\n pages = {257--260},\n location = {Victoria, BC, Canada},\n doi = {http://doi.acm.org/10.1145/1622176.1622223},\n publisher = {ACM},\n pdf = {Papers/dido.pdf},\n venue = {UIST},\n url = {http://projects.csail.mit.edu/exhibit/Dido/},\n cat:multiple = {CHI;Haystack;Semantic Web;Systems;Visualization},\n hideaddress = {New York, NY, USA},\n}"
+		}
 	]
 }
