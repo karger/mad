52a095eb8523c583a9207bf1f824572c62d4fa52
David Karger <karger@mit.edu>
2019-08-02 17:17:10 -0400

    Updated bib.js

--- bib.js
+++ bib.js
@@ -6,7 +6,6 @@
 			"notes": "This is the ancient paper that introduced the idea of active documents.",
 			"bibtex": "@article{Terry:ActiveTioga,\n author = {Terry, Douglas B. and Baker, Donald G.},\n title = {Active tioga documents: an exploration of two paradigms},\n journal = {Electronic Publishing---Origination, Dissemination, and Design},\n volume = {3},\n number = {2},\n year = {1990},\n issn = {0894-3982},\n pages = {105--122},\n publisher = {John Wiley and Sons Ltd.},\n address = {Chichester, UK},\n}\n"
 		},
-		{},
 		{
 			"link": "http://doi.acm.org/10.1145/146486.146547",
 			"title": "EmbeddedButtons: supporting buttons in documents",
